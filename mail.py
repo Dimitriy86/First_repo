d56388885f252236375f439b3885de6283d320cc
dmitr <dmitriyshirin86@gmail.com>
2025-11-19 21:28:43 +0100

    Закриття файлу за допомогою конструкції try-catch

--- mail.py
+++ mail.py
@@ -1,2 +1,7 @@
-print('Hello world!!!')
-print('Hello Git!!!')
+fh = open('test.txt', 'w')
+try:
+    # Виконання операцій з файлом
+    fh.write('Some data')
+finally:
+    # Закриття файлу в блоці finally гарантує, що файл заакривається навідь у разі помилки
+    fh.close()
