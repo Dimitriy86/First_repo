f6b8fdb314f47e1a046001ee678ff39c93db6b4c
dmitr <dmitriyshirin86@gmail.com>
2025-11-22 17:35:35 +0100

    Program of calculation

--- mail.py
+++ mail.py
@@ -1,8 +1,22 @@
-x1 = 10
-y1 = 10
-x2 = 25
-y2 = 25
+# Встановлюємо ціни на продукти
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per-coffee_pack = 4.42
 
-d = ((x2 - x1)**2 + (y2-y1)**2)**.5
+# Кількість кожного продукту
+num_croissants = int(input("Введить кількість круасанів: "))
+num_glasses = int(input("Введить кількість склянок: "))
+num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
 
-print(f"Відстань між точками дорівнює {d}")
+# Обчислення загальної вартості
+total_cost = num*croissants * price_per_croissant + \
+    num_glasses * price_per_glass + \
+    num_coffee_packs * price_per_coffee_pack
+
+# Визначаемо кількість повних доларів і центів
+total_dollars = int(total_cost)
+total_cents = int(total_cost * 100)
+
+# Вивід результату
+print(f"Зальна вартість у повних доларах: {total_dollars} доларів.")
+print(f"Загальна вартість у центах: {total_cents} центів.")
